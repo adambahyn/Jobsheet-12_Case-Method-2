3803fbf4ea1a777f4948cc08c9b53bcc74b61d56
Adammm <adambahymaulana7@gmail.com>
2025-05-30 14:11:53 +0700

    tmbh class

---
+++ TransaksiPengisian.java
@@ -0,0 +1,11 @@
+public class TransaksiPengisian {
+    Kendaraan kendaraan;
+    BBM bbm;
+    double liter, totalBayar;
+
+    public TransaksiPengisian(Kendaraan kendaraan, BBM bbm, double liter) {
+        this.kendaraan = kendaraan;
+        this.bbm = bbm;
+        this.liter = liter;
+    }
+}
